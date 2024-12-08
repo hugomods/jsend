31c38e6c1d8ff3712114ae0322850464244e1100
razonyang <razonyang@gmail.com>
2024-12-08 15:19:45 +0800

    feat: first commit

---
+++ assets/@hugomods/jsend/index.ts
@@ -0,0 +1,48 @@
+type Data = unknown;
+
+interface JSend {
+  status: string;
+  data?: Data;
+  code?: number;
+  message?: string;
+}
+
+type ErrorFunc = {
+  (message: string): JSend;
+  (message: string, code: number): JSend;
+  (message: string, code: number, data: Data): JSend;
+};
+
+export const error: ErrorFunc = (
+  message: string,
+  code?: number,
+  data?: Data,
+): JSend => {
+  const body: JSend = {
+    status: 'error',
+    message,
+  };
+
+  if (code !== undefined) {
+    body.code = code;
+  }
+  if (data !== undefined) {
+    body.data = data;
+  }
+
+  return body;
+};
+
+export const fail = (data?: Data): JSend => {
+  return {
+    status: 'fail',
+    data: data ?? null,
+  };
+};
+
+export const success = (data?: Data): JSend => {
+  return {
+    status: 'success',
+    data: data ?? null,
+  };
+};
