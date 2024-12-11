a0c390006dd3a6785e39d42f2bab058dfd910c7e
razonyang <razonyang@gmail.com>
2024-12-11 13:15:21 +0800

    fix: add JSDoc comments

--- assets/@hugomods/jsend/index.ts
+++ assets/@hugomods/jsend/index.ts
@@ -1,5 +1,14 @@
 type Data = unknown;
 
+/**
+ * Interface that represent a JSend response.
+ *
+ * @interface
+ * @property {string} status - The response status: success, fail or error.
+ * @property {unknown} [data] - The optional response data.
+ * @property {number} [code] - The error code.
+ * @property {string} [message] - The error message.
+ */
 interface JSend {
   status: string;
   data?: Data;
@@ -14,6 +23,13 @@ type ErrorFunc = {
   (message: string, code: number, data: Data, a: string): JSend;
 };
 
+/**
+ * Generate an error response.
+ * @param {string} message - Error message.
+ * @param {number} [code] - Error code.
+ * @param {unknown} [data]
+ * @returns {JSend}
+ */
 export const error: ErrorFunc = (
   message: string,
   code?: number,
@@ -34,6 +50,11 @@ export const error: ErrorFunc = (
   return body;
 };
 
+/**
+ * Generate a fail response.
+ * @param {unknown} [data]
+ * @returns {JSend}
+ */
 export const fail = (data?: Data): JSend => {
   return {
     status: 'fail',
@@ -41,6 +62,11 @@ export const fail = (data?: Data): JSend => {
   };
 };
 
+/**
+ * Generate a success response.
+ * @param {unknown} [data]
+ * @returns {JSend}
+ */
 export const success = (data?: Data): JSend => {
   return {
     status: 'success',
@@ -48,4 +74,5 @@ export const success = (data?: Data): JSend => {
   };
 };
 
+/** Success response that without data. */
 export const SUCCESS: JSend = success();
